ad6066874a21268b5f9f5db70a75589366b16316
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-10-06 19:40:27 +0530

    Create 206Reverselinkedlist.cpp

---
+++ leetcode/206Reverselinkedlist.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode*head){
+        if(head==NULL or head->next==NULL)
+        {
+            return head;
+        }
+        ListNode *p=NULL,*c=head,*a=c->next;
+        while(c!=NULL)
+        {
+            c->next=p;
+            p=c;
+            c=a;
+            if(c->next!=NULL)
+                a=c->next;
+        }
+        return p;
+        
+    }
+};
